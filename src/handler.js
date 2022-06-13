3c9b3688f03a69b1becb48e52b8cc54598c9575b
Fikri Yusrihan <47127883+fikriyusrihan@users.noreply.github.com>
2022-06-13 14:00:58 +0700

    getBookById implementation

--- src/handler.js
+++ src/handler.js
@@ -103,4 +103,30 @@ const getAllBooksHandler = (_, h) => {
   return response;
 };
 
-export {addBookHandler, getAllBooksHandler};
+const getBookByIdHandler = (request, h) => {
+  const {bookId} = request.params;
+
+  const book = books.filter((book) => book.id === bookId)[0];
+
+  if (book !== undefined) {
+    const response = h.response({
+      status: 'success',
+      data: {
+        book,
+      },
+    });
+
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+
+  response.code(404);
+  return response;
+};
+
+export {addBookHandler, getAllBooksHandler, getBookByIdHandler};
